2f360c5e3aa22115806456626f56500935d7dcf1
禚晨晨 <834946220@qq.com>
2017-08-02 19:47:26 +0800

    Add a new python module to generate captcha images. Ref #2

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^captcha/', views.get_captcha)
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import captcha
+from io import BytesIO
 
-# Create your views here.
+def get_captcha(request):
+    f = BytesIO()
+    img, code = captcha.generate_captcha()
+    # 将验证码存在服务器的session中，用于校验
+    request.session['captcha'] = code
+    img.save(f,'PNG')
+    return HttpResponse(f.getvalue())
