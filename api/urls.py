31332fbc6c8023be88f9ef0a27030c930557b192
禚晨晨 <834946220@qq.com>
2017-08-03 12:03:56 +0800

    Set up connections between django and vue using AES_CBC to encrpyt password. Ref #2

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^api/aes', views.aes),
     url(r'^api/captcha-email', views.captcha_email),
     url(r'^api/captcha', views.get_captcha),
     url(r'^api/register', views.register)
--- api/views.py
+++ api/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+# parse json request
 import simplejson
+# generate and send captcha
 from api.captcha import Captcha
 from io import BytesIO
 import base64
+# send email
 from django.core.mail import send_mail
+# create user
 from api.models import User
+# password encrypt and decrypt
+from Crypto.Hash import MD5
+from Crypto.Cipher import AES
+
 
 def get_captcha(request):
     f = BytesIO()
@@ -53,3 +61,36 @@ def captcha_email(request):
             'captcha': captcha
         })
         return response
+
+def aes(request):
+    # 生成秘钥的字符串,可以用验证码
+    needed_string = 'abcdefg'
+    key_md5 = MD5.new()
+    key_md5.update(b'abcdefg')
+    # 可以用于AES的key
+    keyString = key_md5.hexdigest()
+    print(keyString)
+    ivString = keyString[2:18]
+    print(ivString)
+    # python需要使用'\0'来填充,否则加密得到的结果可能会butong
+    # !shurufa huai le !
+    PADDING = '\0'
+    pad_it = lambda s: s + (16 - len(s) % 16) * PADDING
+    data = '666666'
+
+    # jiamiguocheng
+    generator = AES.new(keyString, AES.MODE_CBC, ivString)
+    crypt = generator.encrypt(pad_it(data))
+    cryptedStr = base64.b64encode(crypt).decode()
+    print(cryptedStr)
+
+    # jiemiguocheng
+    generator = AES.new(keyString, AES.MODE_CBC, ivString)
+    # jiema dao bytes geshi
+    recovery = generator.decrypt(crypt)
+    # !!!zhe shi jie mi hui lai de zi fu chuan
+    recovery_string = recovery.decode().rstrip(PADDING)
+    # recovery = recovery.rstrip(PADDING)
+    print(recovery_string)
+    # print(recovery.rstrip(PADDING)
+    return HttpResponse('hello')
