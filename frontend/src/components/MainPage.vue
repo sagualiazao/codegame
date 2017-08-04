7985d4928e1100d9c41da30f8550a88d3365a1d1
樊文杰 <1511415@mail.nankai.edu.cn>
2017-08-04 02:41:36 +0000

    Adding signupwindow

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="main-page">
+    <img src="../assets/logo.png">
+    <br>
+    <el-button type="text" @click="clickTest('game')" size="large">开始游戏</el-button>
+    <h1>这是主页</h1>
     <h1>{{ msg }}</h1>
+    <footer>
+      This is designed by sagualiangzao team
+    </footer>
+
 </div>
 </template>
 
@@ -11,13 +19,21 @@ export default {
         return {
             msg: '看到这行字，说明它正常了'
         }
+    },
+    methods: {
+        clickTest (text) {
+            this.$parent.$store.commit('changeView', text)
+        },
+        handleSelect (index) {
+            this.$parent.$store.commit('changeView', index)
+        }
     }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1 {
     font-weight: normal;
 }
 
