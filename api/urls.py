7033fdda293472de6cb29f1d46c86613e5cde957
禚晨晨 <834946220@qq.com>
2017-08-04 12:04:39 +0800

    Complete the function to reset password if an user forgot his password when login. Ref #7

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^api/test', views.test),
-    url(r'^api/captcha-email', views.captcha_email),
-    url(r'^api/captcha', views.get_captcha),
-    url(r'^api/register', views.register)
+    url(r'^api/test$', views.test),
+    url(r'^api/captcha-email$', views.captcha_email),
+    url(r'^api/captcha$', views.get_captcha),
+    url(r'^api/reset-password-mail$', views.reset_password_email),
+    url(r'^api/reset-password$', views.reset_password),
+    url(r'^api/register$', views.register)
 ]
--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ import api.cbc as CBC
 
 def get_captcha(request):
     '''
-        接受指向'/api/captcha'的GET请求,返回验证码图片的base64编码和对应字符串
+        接受指向'/api/captcha'的GET请求,返回验证码图片的base64编码'img'和对应字符串'captcha'
     '''
     f = BytesIO()
     img, code = Captcha.generate_captcha()
@@ -28,7 +28,7 @@ def get_captcha(request):
 @csrf_exempt
 def register(request):
     '''
-        接受指向'/api/register'的POST请求,在数据库中注册用户,成功的话返回状态码'1'
+        接受指向'/api/register'的POST请求,在数据库中注册用户,成功的话返回'status'状态码'1'
     '''
     req = simplejson.load(request)
     email = req['email']
@@ -41,10 +41,10 @@ def register(request):
     response = JsonResponse({ 'status': '1' })
     return response
 
-@csrf_exempt
-def captcha_email(request):
+def send_email(request, title, msg):
     '''
-        获取请求中的email地址,发送邮件,返回状态码('0'表示发送失败/邮箱已被注册,'1'表示发送成功)和验证码答案
+        获取请求中的'email'地址,发送邮件,标题为'title',消息内容为'msg'\n
+        返回状态码'status'('0'表示当前邮箱尚未注册,'1'表示发送成功)和验证码答案'captcha'
     '''
     req = simplejson.load(request)
     email = req['email']
@@ -54,9 +54,9 @@ def captcha_email(request):
         return response
     else:
         captcha = ''.join(Captcha.string_captcha())
-        msg = '您注册账户: [' + email + '] 的验证码是: ' + captcha
+        msg = '您好,[' + email + ']!\n' + msg + captcha
         mail_status = send_mail(
-            r'[验证码]仨瓜俩枣小组的编程游戏',
+            title,
             msg,
             'sagua_liazao@sohu.com',
             [email],
@@ -68,6 +68,90 @@ def captcha_email(request):
         })
         return response
 
+
+@csrf_exempt
+def captcha_email(request):
+    '''
+        获取请求中的email地址,发送邮件,返回'status'状态码('0'表示发送失败/邮箱已被注册,'1'表示发送成功)和验证码答案'captcha'
+    '''
+    req = simplejson.load(request)
+    email = req['email']
+    users = User.objects.filter(email=email)
+    if len(users) != 0:
+        response = JsonResponse({ 'status': '0'})
+        return response
+    else:
+        captcha = ''.join(Captcha.string_captcha())
+        msg = '您注册账户: [' + email + '] 的验证码是: ' + captcha
+        mail_status = send_mail(
+            r'[验证码]仨瓜俩枣小组的编程游戏-新用户注册',
+            msg,
+            'sagua_liazao@sohu.com',
+            [email],
+            fail_silently=False
+            )
+        response = JsonResponse({
+            'status': '%s'%mail_status,
+            'captcha': captcha
+        })
+        return response
+
+@csrf_exempt
+def reset_password_email(request):
+    '''
+        获取请求中的email地址,发送邮件,返回'status'状态码('0'表示邮箱未被注册,'1'表示发送成功)和验证码答案'captcha'
+    '''
+    req = simplejson.load(request)
+    email = req['email']
+    users = User.objects.filter(email=email)
+    if len(users) == 0:
+        response = JsonResponse({ 'status': '0'})
+        return response
+    else:
+        captcha = ''.join(Captcha.string_captcha())
+        msg = '您重置账户: [' + email + '] 密码的验证码是: ' + captcha
+        mail_status = send_mail(
+            r'[验证码]仨瓜俩枣小组的编程游戏-密码重置',
+            msg,
+            'sagua_liazao@sohu.com',
+            [email],
+            fail_silently=False
+            )
+        response = JsonResponse({
+            'status': '%s'%mail_status,
+            'captcha': captcha
+        })
+        return response
+
+@csrf_exempt
+def reset_password(request):
+    '''
+        接收一个POST请求: request\n
+        包含'email','captcha',加密后的'password'
+        接受请求中的'email'地址和'captcha',将对应账户的密码设为'password'\n
+        修改成功,返回status='1',否则返回'status'='0'
+    '''
+    if request.method == 'GET':
+        return HttpResponse(
+            'This request is designed to reset your password, but you should send a POST request!'
+            )
+    elif request.method == 'POST':
+        req = simplejson.load(request)
+        email = req['email']
+        users = User.objects.filter(email=email)
+        if len(users) == 0:
+            response = JsonResponse({ 'status': '0'})
+            return response
+        else:
+            password_ace = req['password']
+            captcha = req['captcha']
+            password = CBC.decrypt(captcha, password_ace)
+            user = users[0]
+            user.set_password(password)
+            response = JsonResponse({ 'status': '1'})
+            return response
+
 def test(request):
     # 用于测试新的api
     return HttpResponse('hello')
+
