ab38232af1d58d595184ce581ea3fdb9704470e4
xiao <15222617505@163.com>
2017-08-06 19:54:15 +0800

    This is block game basic Ref #10

--- blockly_packaging/第三方js文件.js
+++
@@ -1,4 +0,0 @@
-// 这是一个第三方js文件示例
-function test() {
-    document.getElementById('circle').style.backgroundColor = 'green'
-}
--- frontend/static/part3_editor_file.js
+++ frontend/static/part3_editor_file.js
@@ -1,15 +1,4 @@
-
-
 //第三方function 在这里定义
-function changeHelloWords (words) {
-    document.getElementById('game-h1').innerHTML = words
-}
-
-function setTheCircleColor(color) {
-    if (color == 'blue') {
-        document.getElementById('game-circle').style.backgroundColor = "blue"
-    }
-    else if (color == 'red') {
-        document.getElementById('game-circle').style.backgroundColor = "red"
-    }
+function goRight (step) {
+    return 'this.direct = 1;this.length = '+step+'#\n'
 }
