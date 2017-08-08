4e4294b0695d1aadd914555bd39917580f1cc936
parisiten <1511497@mail.nankai.edu.cn>
2017-08-08 20:16:49 +0800

    fixed the gramma error. Ref #34

--- frontend/static/block_defined/dg_go_up.js
+++ frontend/static/block_defined/dg_go_up.js
@@ -1,19 +1,18 @@
 Blockly.Blocks['dg_go_up'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("go up")
-        .appendField(new Blockly.FieldNumber(0, 0, 100), "step")
-        .appendField("step")
-    this.setPreviousStatement(true, null)
-    this.setNextStatement(true, null)
-    this.setColour(300)
- this.setTooltip("")
- this.setHelpUrl("")
-  }
-};
-Blockly.JavaScript['dg_go_up'] = function(block) {
-  var number_step = block.getFieldValue('step')
-  // TODO: Assemble JavaScript into code variable.
-  var code = number_step+',4,'
-  return code
-};
+    init: function() {
+        this.appendDummyInput()
+        .appendField('go up')
+        .appendField(new Blockly.FieldNumber(0, 0, 100), 'step')
+        .appendField('step')
+        this.setPreviousStatement(true, null)
+        this.setNextStatement(true, null)
+        this.setColour(300)
+        this.setTooltip('')
+        this.setHelpUrl('')
+    }
+}
+Blockly.JavaScript['dg_go_up'] = function (block) {
+    let number_step = block.getFieldValue('step')
+    let code = 'this.length = ' + number_step + ' ; this.direct = 4#'
+    return code
+}
--- frontend/static/part3_editor_file.js
+++ frontend/static/part3_editor_file.js
@@ -1,13 +1,13 @@
 //第三方function 在这里定义
 function goRight (step) {
-    return step+',1'
+    return step + ',1'
 }
 function goLeft (step) {
-    return step+',2'
+    return step + ',2'
 }
 function goUp (step) {
-    return step+',4'
+    return step + ',4'
 }
 function goDown (step) {
-    return step+',3'
+    return step + ',3'
 }
