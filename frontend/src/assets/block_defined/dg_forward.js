446b427f84f877a28413ea183ae1391145e48902
xiao <15222617505@163.com>
2017-08-11 00:12:51 +0800

    Improve go and turn. Ref #46

---
+++ frontend/src/assets/block_defined/dg_forward.js
@@ -0,0 +1,18 @@
+global.Blockly.Blocks['dg_forward'] = {
+    init: function () {
+        this.appendDummyInput()
+        .appendField('move forward')
+        .appendField(new global.Blockly.FieldNumber(0, 0, 100), 'step')
+        .appendField('step')
+        this.setPreviousStatement(true, null)
+        this.setNextStatement(true, null)
+        this.setColour(260)
+        this.setTooltip('')
+        this.setHelpUrl('')
+    }
+}
+global.Blockly.JavaScript['dg_forward'] = function (block) {
+    let numberStep = block.getFieldValue('step')
+    let code = 'go(' + numberStep + ')#'
+    return code
+}
--- frontend/src/components/Atest.vue
+++ frontend/src/components/Atest.vue
@@ -1,16 +1,7 @@
 <template>
 <div class="block-base">
-    <div class="game-area">
-        <canvas id="game-canval" height="640" width="640"></canvas>
-    </div>
-    <div class="tab-plugin">
-        <a class="block-tab tab" id="block-tab">Block</a>
-        <a class="editor-tab tab" @click="editorClick('editor-base')" id="editor-tab">Editor</a>
-        <div class="tab-container" id="block-area"></div>
-        <input type="text" id="code-area" class="code-area">
-        <button class="clean-button">Clean</button>
-        <button class="run-button" @click="blockRunCode()">Run</button>
-    </div>
+    <canvas id="game-canval" height="640" width="640"></canvas>
+    <button @click="dd">cc</button>
 </div>
 </template>
 
@@ -20,7 +11,6 @@ export default {
     name: 'block-base',
     data: function () {
         return {
-            workspace: null,
             pic: null,
             maps: null,
             player: null,
@@ -30,93 +20,16 @@ export default {
             mapHeight: 10,
             mapx: 0,
             mapy: 0,
+            playerx: 0,
+            playery: 0,
+            direct: 1,
+            length: 0,
             divx: 64,
             speed: 1000,
-            tween: null,
-            direct: 2
+            tween: null
         }
     },
     methods: {
-
-        editorClick (index) {
-            this.$parent.$store.commit('changeView', index)
-        },
-        /**
-        *
-        根据当前workspace中的返回值, 通过该函数分解为一个命令列表
-        *
-        @method getCodeList
-        *
-        @for BlockBase.vue
-        *
-        @return {List} 返回一个列表,每个元素为一条命令
-        */
-        getCodeList () {
-            document.LoopTrap = 1000
-            global.Blockly.JavaScript.INFINITE_LOOP_TRAP =
-            'if (--window.LoopTrap === 0) throw "Infinite loop.";\n'
-            let codeString = global.Blockly.JavaScript.workspaceToCode(this.workspace)
-            let codeList = codeString.split('#')
-            return codeList
-        },
-        getTypeOfCode (code) {
-            alert(code)
-            if (code === 'turn(right)') {
-                return 1
-            } else if (code === 'turn(left)') {
-                return 2
-            } else if (code.match(/go(\w*)/)) {
-                return 3
-            } else {
-                return 0
-            }
-        },
-        chooseRightGoFunction (step) {
-            this.tween = createjs.Tween.get(this.player)
-            switch (this.direct) {
-            case 1:
-                this.goUp(step)
-                break
-            case 2:
-                this.goRight(step)
-                break
-            case 3:
-                this.goDown(step)
-                break
-            case 4:
-                this.goLeft(step)
-                break
-            default:
-                alert('something went wrong in chooseRightGoFunction!')
-            }
-        },
-        blockRunCode () {
-            let codeList = this.getCodeList()
-            for (let i = 0; i < codeList.length - 1; i++) {
-                let typeOfCode = this.getTypeOfCode(codeList[i])
-                switch (typeOfCode) {
-                case 0:
-                    alert('Something wrong with the input.')
-                    break
-                case 1:
-                    this.direct = (direct + 1) % 4
-                    break
-                case 2:
-                    this.direct = (direct - 1) % 4
-                    break
-                case 3:
-                    let step = parseInt(codeList[i][3])
-                    this.chooseRightGoFunction(step)
-                    break
-                default:
-                    alert('something went wrong in blockRunCode function!')
-                }
-            }
-        },
-        myUpdateFunction (event) {
-            let code = global.Blockly.JavaScript.workspaceToCode(this.workspace)
-            document.getElementById('code-area').value = code
-        },
         read: async function () {
             let response = await fetch('api/read-map?mapid=' + this.mapId, {
                 method: 'get',
@@ -146,7 +59,7 @@ export default {
             this.mapy = 0
             this.playerx = 0
             this.playery = 0
-            this.direct = 2
+            this.direct = 1
             this.length = 0
             this.divx = 64
             var canvas = document.getElementById('game-canval')
@@ -192,7 +105,7 @@ export default {
             this.player = new createjs.Sprite(spritesheet)
             this.player.x = this.playerx
             this.player.y = this.playery
-            this.player.gotoAndPlay('runRight')
+            this.player.gotoAndStop(8)
             this.stage.addChild(this.player)
             this.tween = createjs.Tween.get(this.player)
             createjs.Ticker.addEventListener('tick', this.stage)
@@ -201,6 +114,7 @@ export default {
             var now = new Date()
             var exitTime = now.getTime() + numberMillis
             while (true) {
+                console.log(1)
                 now = new Date()
                 if (now.getTime() > exitTime) {
                     return
@@ -301,126 +215,25 @@ export default {
             }
             this.tween.call(this.getPlay, [2]).to({y: playery}, this.speed).call(this.getStop, [2])
             this.player.y = playery
+        },
+        dd () {
+            this.tween = createjs.Tween.get(this.player)
+            this.goRight(5)
+            console.log(this.tween)
+            // this.goDown(8)
+            // this.goUp(4)
+            // // this.wait(1000)
+            // this.goRight(5)
+            // this.goLeft(7)
         }
     },
     mounted: function () {
-        // require('../assets/block_defined/dg_go_up.js')
-        // require('../assets/block_defined/dg_go_down.js')
-        // require('../assets/block_defined/dg_go_left.js')
-        // require('../assets/block_defined/dg_go_right.js')
-        require('../assets/block_defined/dg_forward.js')
-        require('../assets/block_defined/dg_turn_direction.js')
-        let toolBox = require('../../src/assets/js/blockly_const_list.js')
-        this.workspace = global.Blockly.inject('block-area', {
-            toolbox: toolBox,
-            grid: {
-                spacing: 20,
-                length: 3,
-                colour: '#ccc',
-                snap: true,
-                trashcan: true
-            },
-            zoom: {
-                controls: true,
-                wheel: true,
-                startScale: 1.0,
-                maxScale: 3,
-                minScale: 0.3,
-                scaleSpeed: 1.2,
-                trashcan: true
-            }
-        })
-        this.workspace.addChangeListener(this.myUpdateFunction)
         this.init()
+        // this.goDown(8)
+        // this.goUp(4)
+        // // this.wait(1000)
+        // this.goRight(5)
+        // this.goLeft(7)
     }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.block-base {
-    margin: 0 auto;
-    width: 1300px;
-    height: 600px;
-    background-color: #FFEC8B;
-}
-.game-area {
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    position: absolute;
-    left: 30px;
-    width: 650px;
-    height: 600px;
-    padding: 10px;
-    border: solid 1px;
-}
-.tab-plugin {
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    position: absolute;
-    right: 30px;
-    width: 650px;
-    height: 600px;
-    border: solid 1px;
-}
-.tab-plugin .tab-container {
-    position: absolute;
-    top: 30px;
-    left: 0;
-    width: 100%;
-    height: 400px;
-    opacity: 1;
-    border: solid 1px;
-}
-.tab-plugin .code-area {
-    position: absolute;
-    top: 430px;
-    left: 0;
-    width: 100%;
-    height: 140px;
-    opacity: 1;
-    border: solid 1px;
-}
-.tab {
-    position: absolute;
-    display: inline-block;
-    height: 30px;
-    weight: 50px;
-    line-height: 30px;
-    text-align: center;
-    color: black;
-    cursor: pointer;
-    border: solid 1px;
-}
-.block-tab {
-    left: 0;
-    background: #FFEC8B;
-}
-.editor-tab {
-    left: 60px;
-    background: #D1EEEE;
-}
-.run-button {
-    position: absolute;
-    top: 570px;
-    right: 0;
-    height:30px;
-    width: 40px;
-    text-align: center;
-    background: #272822;
-    color: #fff;
-    cursor: pointer;
-}
-.clean-button {
-    position: absolute;
-    top: 570px;
-    right: 50px;
-    height:30px;
-    text-align: center;
-    background: #272822;
-    color: #fff;
-    cursor: pointer;
-}
-</style>
