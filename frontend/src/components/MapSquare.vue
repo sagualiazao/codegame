68dcf09524333543b5f293a85da4303089ddb456
zuo <2438627331@qq.com>
2017-08-20 21:00:58 +0800

    Collect maps in MapSquare. Ref #132

--- frontend/src/components/MapSquare.vue
+++ frontend/src/components/MapSquare.vue
@@ -2,17 +2,20 @@
 <div class="map-square">
     <el-tabs ref="tabs" v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="地图广场" name="first" class="map-square-tab">
-            <div v-for="map in mapList">
-                <div class="caption">
-                    <p class="mapname">地图名称: {{ map[1] }}</p>
-                    <p class="image"><img :src="map[3]" alt="显示错误"></p>
-                    <p class="author">地图作者: {{ map[2] }}</p>
-                    <p class="remarks">地图说明: {{ map[4] }}</p>
-                    <div v-if="map[5] === false">
-                        <i class="el-icon-star-off" @click="changeFavor(map, true)" title="点击收藏地图">收藏</i>
-                    </div>
-                    <div v-else>
-                        <i class="el-icon-star-on" @click="changeFavor(map, false)">已收藏</i>
+            <div v-for="(map, index) in mapList">
+                <div class="map-picture">
+                    <a href="#BlockBase"><img :src="map[3]" class="image" alt="显示错误">
+                    <span class="tips">点击图片进入此游戏</span></a>
+                    <div class="caption">
+                        <span class="mapname">地图名: {{ map[1] }}</span>
+                        <span class="author">作者: {{ map[2] }}</span><br>
+                        <span class="remarks">说明: {{ map[4] }}</span>
+                        <div v-if="map[5] === false">
+                            <i class="el-icon-star-off" @click="changeFavor(map, true)" title="点击收藏地图">收藏</i>
+                        </div>
+                        <div v-else>
+                            <i class="el-icon-star-on" @click="changeFavor(map, false)" title="点击取消收藏">已收藏</i>
+                        </div>
                     </div>
                     <hr>
                 </div>
@@ -21,22 +24,25 @@
         </el-tab-pane>
         <el-tab-pane label="我发布的地图" name="second" class="published-map-tab">
             <div v-for="map in publishedMapList">
-                <div class="caption">
-                    <p class="mapname">地图名称: {{ map[1] }}</p>
-                    <p class="image"><img :src="map[2]" alt="显示错误"></p>
-                    <p class="remarks">地图说明: {{ map[3] }}</p>
-                    <hr>
+                <div class="map-picture">
+                    <a href="#BlockBase"><img :src="map[2]" class="image" alt="显示错误"></a>
+                    <div class="caption">
+                        <span class="mapname">地图名: {{ map[1] }}</span><br>
+                        <span class="remarks">说明: {{ map[3] }}</span>
+                    </div>
                 </div>
             </div>
         </el-tab-pane>
         <el-tab-pane label="我收藏的地图" name="third" class="favorite-map-tab">
             <div v-for="map in favoriteMapList">
-                <div class="caption">
-                    <p class="mapname">地图名称: {{ map[1] }}</p>
-                    <p class="image"><img :src="map[3]" alt="显示错误"></p>
-                    <p class="author">地图作者: {{ map[2] }}</p>
-                    <p class="remarks">地图说明: {{ map[4] }}</p>
-                    <hr>
+                <div class="map-picture">
+                    <a href="#BlockBase"><img :src="map[3]" class="image" alt="显示错误"></a>
+                    <div class="caption">
+                        <span class="mapname">地图名: {{ map[1] }}</span>
+                        <span class="author">作者: {{ map[2] }}</span><br>
+                        <span class="remarks">说明: {{ map[4] }}</span>
+                        <i class="el-icon-star-on" @click="changeFavor(map, false)" title="点击取消收藏">已收藏</i>
+                    </div>
                 </div>
             </div>
         </el-tab-pane>
@@ -77,12 +83,13 @@ export default {
         this.readFavoriteMapList()
         this.readPublishedMapList()
     },
+    compute: {
+
+    },
     methods: {
         handleClick (tab, event) {
         },
         playClick () {
-        },
-        collect () {
         },
         changeFavor (map, status) {
             map[5] = status
@@ -147,6 +154,8 @@ export default {
             if (await obj.status === '1') {
                 // TODO: 更改按钮显示状态
             }
+            this.readMapList()
+            this.readFavoriteMapList()
         },
         cancelPublishStatus: async function (id) {
             // 取消地图发布状态
@@ -172,17 +181,55 @@ h1 {
 .el-icon-star-on {
     color: orange;
 }
+div.map-picture {
+    border: ridge 2px #ADD8E6;
+    text-align: center;
+    width: auto;
+    height: auto;
+    float: left;
+    margin: 3px;
+    display: block;
+    box-sizing: border-box;
+    transition: all .4s ease-in-out;
+    overflow: hidden;
+}
+.caption {
+    text-align: center;
+    font-weight: normal;
+    width: 150px;
+    font-size: 12px;
+    margin: 10px 5px 10px 5px;
+  }
+  .map-picture a {
+      display: block;
+  }
+  .map-picture a span {
+      display: none;
+      transform: scale(1);
+  }
+  .map-picture a:hover span {
+      display: block;
+      text-align: center;
+      width: 80%;
+      vertical-align: center;
+      position: absolute;
+      overflow: hidden;
+  }
 img {
-    width: 300px;
-    height: 200px;
+    width: auto;
+    height: auto;
+    display: block;
+    margin: 3px;
 }
 img {
     opacity: 0.4;
     filter: alpha(opacity = 40); /* For IE8 and earlier */
+    transition: all 0.5s;
 }
 img:hover {
     opacity: 1.0;
     filter: alpha(opacity = 100); /* For IE8 and earlier */
+    transform: scale(1.1);
 }
 button {
     display: inline-block;
