f3c69f7be45b1267379127aee6c3e9934c53ef39
禚晨晨 <834946220@qq.com>
2017-08-23 00:13:19 +0800

    Add a shell file to help to write game levels into database. Close #165

---
+++ 写入关卡.sh
@@ -0,0 +1,8 @@
+use doublegame;
+delete from api_gamelevels;
+load data local infile \"./levelinfo\" ignore into table doublegame.api_gamelevels FIELDS terminated by '$\n' LINES starting by '{\n' terminated by '}\n' (map_id, map, name, tips, codes, mode)
+" > import.sql
+mysql -h localhost -u root -pvagrant < ./import.sql
+rm import.sql
+echo 关卡信息已写入
+
