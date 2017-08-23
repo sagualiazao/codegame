4a680ec18639815a98051222926ae60c1017b6ea
禚晨晨 <834946220@qq.com>
2017-08-23 14:07:46 +0800

    Fix the write_levelinfo shell. Ref #167

--- write_levelinfo.sh
+++ write_levelinfo.sh
@@ -1,3 +1,5 @@
+touch import.sql
+echo """
 use doublegame;
 delete from api_gamelevels;
 load data local infile \"./levelinfo\" ignore into table doublegame.api_gamelevels FIELDS terminated by '$\n' LINES starting by '{\n' terminated by '}\n' (map_id, map, name, tips, codes, mode)
